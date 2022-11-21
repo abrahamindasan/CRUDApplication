02d427bedccc6f047d354ca20524a9d063ea49e9
abrahamindasan <112670209+abrahamindasan@users.noreply.github.com>
2022-11-22 00:00:35 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,42 +1,19 @@
 <?php
-    include_once("dbConnection/mysqlconfig_connection.php");
-    include_once("functions/fetch.php");
+    
+    //this is where the page starts
+
+    //start session
+    session_start();
+
+    //check if user is login already otherwise send to login page
+    if (isset($_SESSION['user_type']) == 'admin'){
+        header('location: admin/dashboard.php');
+    }
+    else if (isset($_SESSION['user_type']) == 'staff'){
+        header('location: faculty/faculty.php');
+    }
+    else{
+        header('location: login/login.php');
+    }
+
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-    <title>Sample CRUD</title>
-    </head>
-
-    <body>
-        <h1>My Subjects</h1>
-        <a href='forms/addform.php'>Add Subject</a><br/><br/>
-        <table width='100%'>
-        <tr>
-            <td>ID</td>
-            <td>Subject Code</td>
-            <td>Subject Name</td>
-            <td>Syllabus ID</td>
-            <td>Syllabus Code</td>
-            <td>Syllabus Author</td>
-            <td>Action</td>
-        </tr>
-        <?php
-            while($res = mysqli_fetch_array($result)){
-                echo "<tr>";
-                echo "<td>".$res['subject_id']."</td>";
-                echo "<td>".$res['subject_code']."</td>";
-                echo "<td>".$res['subject_name']."</td>";
-                echo "<td>".$res['syllabus id']."</td>";
-                echo "<td>".$res['syllabus_code']."</td>";
-                echo "<td>".$res['syllabus_author']."</td>";
-                echo "<td><a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
-                        <a href=\"functions/delete.php?id=$res[subject_id]\"
-                        onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
-                echo "</tr>"
-            }
-        ?>
-        </table>
-    </body>
-
-</html>
